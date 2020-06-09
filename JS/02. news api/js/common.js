2a8613173f4e2101ab49c1d348238455f414cb87
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2020-06-09 19:10:46 +0300

    Continue with news

--- JS/02. news api/js/common.js
+++ JS/02. news api/js/common.js
@@ -1,12 +1,31 @@
 window.addEventListener("load", Init);
 
 function Init() {
-  Requests("ua", "sports", sports);
+  let category = [
+    "sports",
+    "entertainment",
+    "health",
+    "science",
+    "technology",
+    "business",
+  ];
+
+  let callbackFunctions = [
+    { news: sportsNews },
+    { news: entertaimentNews },
+    { news: healthNews },
+    { news: scinceNews },
+    { news: technologyNews },
+    { news: businessNews },
+  ];
+
+  for (let i = 0; i < category.length; i++) {
+    Requests("ua", category[i], callbackFunctions[i].news);
+  }
 }
 
 function Requests(country, category, callback) {
   const URL = `http://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=18f1c87e444741aca30db0a569bba999`;
-  console.log(URL);
   fetch(URL)
     .then((data) => {
       return data.json();
@@ -17,10 +36,25 @@ function Requests(country, category, callback) {
     .catch((err) => console.log(err));
 }
 
-function sports(news) {
-  console.log("sports ", news);
+function sportsNews(news) {
+  console.log("sportsNews ", news);
 }
 
-function science(news) {
-  console.log("science", news);
+function entertaimentNews(news) {
+  console.log("entertaimentNews", news);
+}
+
+function healthNews(news) {
+  console.log("healthNews", news);
+}
+
+function scinceNews(news) {
+  console.log("scinceNews", news);
+}
+function technologyNews(news) {
+  console.log("technologyNews", news);
+}
+
+function businessNews(news) {
+  console.log("businessNews", news);
 }
